f5c4e9678b0a0a15c4f8d5a7a090a089160beda4
Alex <tektonikboy98@gmail.com>
2020-11-24 12:28:39 +0600

    Deploy

--- bilim/settings.py
+++ bilim/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 import dj_database_url
+
 from decouple import config, Csv
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +144,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
